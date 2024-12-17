8297ab6db46da5b07da9364ba234f9b7f34337f4
sebastienjeanfavre <sjeanfavre@gmail.com>
2024-12-17 16:26:06 +0100

    added rankings in staging

---
+++ src/dml/staging/ranking.sql
@@ -0,0 +1,16 @@
+-- CREATE OR REPLACE TABLE staging.ranking AS 
+SELECT 
+    PARSE_JSON(t.json):ranking_id::NUMBER AS ranking_id,
+    PARSE_JSON(t.json):ranking_name::VARCHAR(256) AS ranking_name,
+    PARSE_JSON(t.json):ranking_cat_id::NUMBER AS ranking_cat_id,
+    PARSE_JSON(t.json):ranking_cat_name::VARCHAR(256) AS ranking_cat_name,
+    PARSE_JSON(t.json):region_id::NUMBER AS region_id,
+    PARSE_JSON(t.json):region_name::VARCHAR(256) AS region_name,
+    PARSE_JSON(t.json):week::VARCHAR(256) AS week,
+    PARSE_JSON(t.json):published::TIMESTAMP_NTZ AS published,
+    PARSE_JSON(t.json):_metadata.timestamp::TIMESTAMP_NTZ AS load_ts
+FROM TABLE(world_triathlon.staging.get_json_all_pages(
+    'https://api.triathlon.org/v1/rankings', 
+    '0201b661afadf43392e4c7dcaed533fe ')) t
+WHERE PARSE_JSON(t.json):_metadata.status_code = 200;
+
---
+++ src/dml/staging/ranking_details.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE TABLE staging.ranking_details AS 
+SELECT 
+    r.ranking_id,
+    f.value:athlete_id::NUMBER AS athlete_id,
+    f.value:rank::NUMBER AS rank,
+    f.value:total::NUMBER AS total,
+    f.value:total_current_period::NUMBER AS total_current_period,
+    f.value:events_current_period::NUMBER AS events_current_period,
+    f.value:scores_current_period::ARRAY AS scores_current_period,
+    f.value:total_previous_period::NUMBER AS total_previous_period,
+    f.value:last_rank::NUMBER AS last_rank,
+    f.value:last_total::NUMBER AS last_total,
+    f.value:events_previous_period::NUMBER AS events_previous_period,
+    f.value:scores_previous_period::ARRAY AS scores_previous_period,
+    f.value:updated_at::TIMESTAMP_NTZ AS updated_at,
+    PARSE_JSON(t.json):_metadata.timestamp::TIMESTAMP_NTZ AS load_ts
+FROM staging.ranking r
+CROSS JOIN TABLE(world_triathlon.staging.get_json_all_pages(
+    'https://api.triathlon.org/v1/rankings/'|| r.ranking_id, 
+    '0201b661afadf43392e4c7dcaed533fe ')) t,
+LATERAL FLATTEN(input => PARSE_JSON(t.json):rankings) f
+WHERE PARSE_JSON(t.json):_metadata.status_code = 200

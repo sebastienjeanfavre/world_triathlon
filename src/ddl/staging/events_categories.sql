5bd211116c53eeaabf1e23b7442ca6c3cfff368d
sebastienjeanfavre <sjeanfavre@gmail.com>
2024-12-19 15:07:42 +0100

    changed to create or replace table for categories

--- src/ddl/staging/events_categories.sql
+++ src/ddl/staging/events_categories.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS staging.event_categories AS 
+CREATE OR REPLACE TABLE staging.event_categories AS 
 SELECT
     PARSE_JSON(json):cat_id::NUMBER AS cat_id,
     PARSE_JSON(json):cat_name::VARCHAR(256) AS cat_name,
     PARSE_JSON(json):cat_order::NUMBER AS cat_order,
     PARSE_JSON(json):cat_parent_id::NUMBER AS cat_parent_id,
     PARSE_JSON(json):cat_slug::VARCHAR(256) AS cat_slug
-FROM TABLE(get_json_all_pages('https://api.triathlon.org/v1/events/categories', 
+FROM TABLE(staging.get_json_all_pages('https://api.triathlon.org/v1/events/categories', 
                                 '0201b661afadf43392e4c7dcaed533fe '))
--- src/ddl/staging/events_specifications.sql
+++ src/ddl/staging/events_specifications.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS staging.event_specifications AS 
+CREATE OR REPLACE TABLE staging.event_specifications AS 
 SELECT
     PARSE_JSON(json):cat_id::NUMBER AS cat_id,
     PARSE_JSON(json):cat_name::VARCHAR(256) AS cat_name,
@@ -6,5 +6,5 @@ SELECT
     PARSE_JSON(json):cat_parent_id::NUMBER AS cat_parent_id,
     PARSE_JSON(json):cat_slug::VARCHAR(256) AS cat_slug,
     PARSE_JSON(json):_metadata.timestamp::TIMESTAMP_NTZ AS load_ts
-FROM TABLE(get_json_all_pages('https://api.triathlon.org/v1/events/specifications?show_children=true', 
+FROM TABLE(staging.get_json_all_pages('https://api.triathlon.org/v1/events/specifications?show_children=true', 
                               '0201b661afadf43392e4c7dcaed533fe '))

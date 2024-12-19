581147605bc4a4fd8f8d4b73116e29e5756f824b
sebastienjeanfavre <sjeanfavre@gmail.com>
2024-12-19 14:26:32 +0100

    removed comments

--- orchestration/sp_refresh_datamart.sql
+++ orchestration/sp_refresh_datamart.sql
@@ -5,10 +5,7 @@ EXECUTE AS CALLER
 AS
 $$
 BEGIN
-    
-    -- Refresh events, programs and results (not athletes)
     ALTER GIT REPOSITORY public.git_repo_stage_world_triathlon FETCH;
-    -- All events are loaded everytime
     EXECUTE IMMEDIATE FROM @public.git_repo_stage_world_triathlon/branches/main/src/dml/datamart/dim_event.sql;
     EXECUTE IMMEDIATE FROM @public.git_repo_stage_world_triathlon/branches/main/src/dml/datamart/dim_program.sql;
     EXECUTE IMMEDIATE FROM @public.git_repo_stage_world_triathlon/branches/main/src/dml/datamart/fct_results.sql;

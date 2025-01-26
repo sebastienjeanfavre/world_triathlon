b5945f1bf3df36b3d7347f30f6648da77aeebec6
sebastienjeanfavre <sjeanfavre@gmail.com>
2025-01-26 14:16:55 +0100

    added comments to execute and alter tasks

--- orchestration/task_refresh_datamart.sql
+++ orchestration/task_refresh_datamart.sql
@@ -3,3 +3,5 @@ WAREHOUSE = compute_wh
 AFTER orchestration.task_refresh_staging
 AS
     CALL staging.sp_refresh_datamart();
+
+-- ALTER TASK orchestration.task_refresh_datamart RESUME;
--- orchestration/task_refresh_staging.sql
+++ orchestration/task_refresh_staging.sql
@@ -3,3 +3,10 @@ WAREHOUSE = compute_wh
 SCHEDULE = 'USING CRON 0 0 * * 3 UTC'
 AS
     CALL staging.sp_refresh_staging();
+
+-- EXECUTE TASK orchestration.task_refresh_staging;
+-- ALTER TASK orchestration.task_refresh_staging RESUME;
+-- select max(load_ts) from datamart.dim_program;
+
+-- -- SELECT * FROM TABLE(information_schema.task_history())
+-- ORDER BY query_start_time DESC;

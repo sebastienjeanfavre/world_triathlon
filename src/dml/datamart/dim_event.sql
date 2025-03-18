07210baba9b409f84ac1b40a9e127642bc55864a
sebastienjeanfavre <sjeanfavre@gmail.com>
2025-03-18 22:09:22 +0100

    adapted datamart to new fields

--- src/dml/datamart/dim_event.sql
+++ src/dml/datamart/dim_event.sql
@@ -2,13 +2,17 @@ INSERT OVERWRITE INTO datamart.dim_event
 SELECT 
     event_id,
     event_title,
+    event_venue,
+    event_status
     event_date,
     event_finish_date,
     event_edit_date,
-    event_country_id,
     event_country,
     event_region_name,
-    specifications,
+    event_latitude,
+    event_longitude,
+    event_categories
+    event_specifications,
     triathlonlive,
     load_ts,
 FROM staging.events

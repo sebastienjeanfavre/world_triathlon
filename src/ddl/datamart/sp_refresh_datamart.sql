762fa5bb1625bb59bf26d679b7a39778691f41a8
sebastienjeanfavre <sjeanfavre@gmail.com>
2025-04-17 20:26:57 +0200

    added logs

--- src/ddl/datamart/sp_refresh_datamart.sql
+++ src/ddl/datamart/sp_refresh_datamart.sql
@@ -4,23 +4,43 @@ LANGUAGE SQL
 EXECUTE AS CALLER
 AS
 $$
+DECLARE
+    start_ts TIMESTAMP := CURRENT_TIMESTAMP();
+    last_ts TIMESTAMP;
 BEGIN
-    SYSTEM$LOG('INFO', 'Fetching remote repo');
+    SYSTEM$LOG('INFO', start_ts || ' - sp_refresh_datamart START');
+
+    -- Fetch Git repo
+    last_ts := CURRENT_TIMESTAMP();
     ALTER GIT REPOSITORY orchestration.git_repo_stage_world_triathlon FETCH;
-    SYSTEM$LOG('INFO', 'Executing src/dml/datamart/dim_event.sql');
+    SYSTEM$LOG('INFO', 'Git repo fetched in ' || DATEDIFF('second', last_ts, CURRENT_TIMESTAMP()) || ' seconds');
+
+    -- dim_event.sql
+    last_ts := CURRENT_TIMESTAMP();
     EXECUTE IMMEDIATE FROM @orchestration.git_repo_stage_world_triathlon/branches/main/src/dml/datamart/dim_event.sql;
-    SYSTEM$LOG('INFO', 'Executing src/dml/datamart/dim_program.sql');
+    SYSTEM$LOG('INFO', 'dim_event.sql executed in ' || DATEDIFF('second', last_ts, CURRENT_TIMESTAMP()) || ' seconds');
+
+    -- dim_program.sql
+    last_ts := CURRENT_TIMESTAMP();
     EXECUTE IMMEDIATE FROM @orchestration.git_repo_stage_world_triathlon/branches/main/src/dml/datamart/dim_program.sql;
-    SYSTEM$LOG('INFO', 'Executing src/dml/datamart/fct_results.sql');
+    SYSTEM$LOG('INFO', 'dim_program.sql executed in ' || DATEDIFF('second', last_ts, CURRENT_TIMESTAMP()) || ' seconds');
+
+    -- fct_results.sql
+    last_ts := CURRENT_TIMESTAMP();
     EXECUTE IMMEDIATE FROM @orchestration.git_repo_stage_world_triathlon/branches/main/src/dml/datamart/fct_results.sql;
-    SYSTEM$LOG('INFO', 'Executing src/dml/datamart/fct_ranking.sql');
+    SYSTEM$LOG('INFO', 'fct_results.sql executed in ' || DATEDIFF('second', last_ts, CURRENT_TIMESTAMP()) || ' seconds');
+
+    -- fct_ranking.sql
+    last_ts := CURRENT_TIMESTAMP();
     EXECUTE IMMEDIATE FROM @orchestration.git_repo_stage_world_triathlon/branches/main/src/dml/datamart/fct_ranking.sql;
-    
-    RETURN 'Successful refresh at ' || CURRENT_TIMESTAMP();
+    SYSTEM$LOG('INFO', 'fct_ranking.sql executed in ' || DATEDIFF('second', last_ts, CURRENT_TIMESTAMP()) || ' seconds');
+
+    -- Procedure completed
+    SYSTEM$LOG('INFO', CURRENT_TIMESTAMP() || ' - sp_refresh_datamart END');
+    RETURN 'Successful datamart refresh in ' || DATEDIFF('second', start_ts, CURRENT_TIMESTAMP()) || ' seconds';
 
 EXCEPTION
     WHEN OTHER THEN
-        -- Log any errors encountered during execution
         SYSTEM$LOG('ERROR', 'Error encountered: ' || SQLERRM);
         RETURN 'Error encountered: ' || SQLERRM;
 END;
--- src/ddl/staging/sp_refresh_staging.sql
+++ src/ddl/staging/sp_refresh_staging.sql
@@ -4,33 +4,54 @@ LANGUAGE SQL
 EXECUTE AS CALLER
 AS
 $$
+DECLARE
+    start_ts TIMESTAMP := CURRENT_TIMESTAMP();
+    last_ts TIMESTAMP;
 BEGIN
-    SYSTEM$LOG('INFO', 'Fetching remote repo');
+    -- Total start
+    SYSTEM$LOG('INFO', start_ts || ' - sp_refresh_staging START');
+
+    -- 1) Fetch remote Git repository
+    last_ts := CURRENT_TIMESTAMP();
     ALTER GIT REPOSITORY orchestration.git_repo_stage_world_triathlon FETCH;
-    -- All events are loaded everytime
-    SYSTEM$LOG('INFO', 'Executing src/dml/staging/events.sql');
+    SYSTEM$LOG('INFO', 'Git repo fetched in ' || DATEDIFF('second', last_ts, CURRENT_TIMESTAMP()) || ' seconds');
+
+    -- 2) events.sql
+    last_ts := CURRENT_TIMESTAMP();
     EXECUTE IMMEDIATE FROM @orchestration.git_repo_stage_world_triathlon/branches/main/src/dml/staging/events.sql;
-    -- Programs are refreshed if event_date >= addmonth(current_date - 1)
-    SYSTEM$LOG('INFO', 'Executing src/dml/staging/programs.sql');
+    SYSTEM$LOG('INFO','events.sql executed in ' || DATEDIFF('second', last_ts, CURRENT_TIMESTAMP()) || ' seconds');
+
+    -- 3) programs.sql
+    last_ts := CURRENT_TIMESTAMP();
     EXECUTE IMMEDIATE FROM @orchestration.git_repo_stage_world_triathlon/branches/main/src/dml/staging/programs.sql;
-    -- Programs are refreshed if event_date >= addmonth(current_date - 1)
-    SYSTEM$LOG('INFO', 'Executing src/dml/staging/programs_results.sql');
+    SYSTEM$LOG('INFO','programs.sql executed in ' || DATEDIFF('second', last_ts, CURRENT_TIMESTAMP()) || ' seconds');
+
+    -- 4) programs_results.sql
+    last_ts := CURRENT_TIMESTAMP();
     EXECUTE IMMEDIATE FROM @orchestration.git_repo_stage_world_triathlon/branches/main/src/dml/staging/programs_results.sql;
-    -- Get start lists for future events
-    SYSTEM$LOG('INFO', 'Executing src/dml/staging/programs_entries.sql');
+    SYSTEM$LOG('INFO','programs_results.sql executed in ' || DATEDIFF('second', last_ts, CURRENT_TIMESTAMP()) || ' seconds');
+
+    -- 5) programs_entries.sql
+    last_ts := CURRENT_TIMESTAMP();
     EXECUTE IMMEDIATE FROM @orchestration.git_repo_stage_world_triathlon/branches/main/src/dml/staging/programs_entries.sql;
-    -- Refresh rankings
-    SYSTEM$LOG('INFO', 'Executing src/dml/staging/ranking.sql');
+    SYSTEM$LOG('INFO','programs_entries.sql executed in ' || DATEDIFF('second', last_ts, CURRENT_TIMESTAMP()) || ' seconds');
+
+    -- 6) ranking.sql
+    last_ts := CURRENT_TIMESTAMP();
     EXECUTE IMMEDIATE FROM @orchestration.git_repo_stage_world_triathlon/branches/main/src/dml/staging/ranking.sql;
-    -- Refresh ranking details
-    SYSTEM$LOG('INFO', 'Executing src/dml/staging/ranking_details.sql');
+    SYSTEM$LOG('INFO','ranking.sql executed in ' || DATEDIFF('second', last_ts, CURRENT_TIMESTAMP()) || ' seconds');
+
+    -- 7) ranking_details.sql
+    last_ts := CURRENT_TIMESTAMP();
     EXECUTE IMMEDIATE FROM @orchestration.git_repo_stage_world_triathlon/branches/main/src/dml/staging/ranking_details.sql;
-    
-    RETURN 'Successful refresh at ' || CURRENT_TIMESTAMP();
+    SYSTEM$LOG('INFO','ranking_details.sql executed in ' || DATEDIFF('second', last_ts, CURRENT_TIMESTAMP()) || ' seconds');
+
+    -- Total end
+    SYSTEM$LOG('INFO', CURRENT_TIMESTAMP() || ' - sp_refresh_staging END');
+    RETURN 'Successful staging refresh in ' || DATEDIFF('second', start_ts, CURRENT_TIMESTAMP()) || ' seconds';
 
 EXCEPTION
     WHEN OTHER THEN
-        -- Log any errors encountered during execution
         SYSTEM$LOG('ERROR', 'Error encountered: ' || SQLERRM);
         RETURN 'Error encountered: ' || SQLERRM;
 END;

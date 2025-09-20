a47711f76934de81e4473b23dad1408824da4724
sebastienjeanfavre <sjeanfavre@gmail.com>
2025-09-20 08:46:49 +0200

    daily refresh on

--- orchestration/task_refresh_staging.sql
+++ orchestration/task_refresh_staging.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE TASK orchestration.task_refresh_staging
 WAREHOUSE = compute_wh
-SCHEDULE = 'USING CRON 0 1 * * 2 UTC'
+SCHEDULE = 'USING CRON 0 1 * * * UTC'
 AS
     CALL staging.sp_refresh_staging()
 ;

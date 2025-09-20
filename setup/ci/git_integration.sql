806ebaa8da99786e4a8eb0a0f20e01aa134ff106
sebastienjeanfavre <sjeanfavre@gmail.com>
2025-09-20 09:09:57 +0200

    added API_USER_AUTHENTICATION

--- setup/ci/git_integration.sql
+++ setup/ci/git_integration.sql
@@ -1,14 +1,15 @@
 -- 1 Create API integration
-CREATE OR REPLACE API INTEGRATION git_api_integration
+CREATE OR REPLACE API INTEGRATION apii_git_world_triathlon
   API_PROVIDER = git_https_api
   API_ALLOWED_PREFIXES = ('https://github.com/sebastienjeanfavre')
+  API_USER_AUTHENTICATION = (TYPE = SNOWFLAKE_GITHUB_APP)
   ENABLED = TRUE;
 
-desc integration git_api_integration;
+desc integration apii_git_world_triathlon;
 
 -- 2 create git repo obj
-CREATE OR REPLACE GIT REPOSITORY orchestration.git_repo_stage_world_triathlon
-  API_INTEGRATION = git_api_integration
+CREATE OR REPLACE GIT REPOSITORY git_repo_stage_world_triathlon
+  API_INTEGRATION = apii_git_world_triathlon
   ORIGIN = 'https://github.com/sebastienjeanfavre/world_triathlon.git';
 
 ls @git_repo_stage_world_triathlon/branches/main/;
